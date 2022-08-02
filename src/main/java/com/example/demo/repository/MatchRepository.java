5c7533edc40a8c58274011fbf30545c79f095394
Abhishek Kumar <abhishekshanu619@gmail.com>
2022-08-02 14:22:39 +0530

    Match Page Added: React

--- src/main/java/com/example/demo/repository/MatchRepository.java
+++ src/main/java/com/example/demo/repository/MatchRepository.java
@@ -1,16 +1,26 @@
 package com.example.demo.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.model.Match;
 
 public interface MatchRepository extends CrudRepository<Match, Long>{
 	
 	List<Match> findByTeam1OrTeam2OrderByDateDesc(String team1, String team2,Pageable pageable);
+	
+	@Query("select m  from Match m where (m.team1=:teamName or m.team2=:teamName) and m.date between :startDate and :endDate order by date desc")
+	List<Match> getMatchesByTeamBetweenDates(
+			@Param("teamName") String teamName,
+			@Param("startDate") LocalDate startDate,
+			@Param("endDate") LocalDate endDate);
+	
 	default List<Match> findLatestMatchesByTeam(String teamName, int count){
 		return findByTeam1OrTeam2OrderByDateDesc(teamName,teamName,PageRequest.of(0, count));
 	}
